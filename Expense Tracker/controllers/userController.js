8e7255a9cf5d1116623ada6c1548207d221ddf74
THODETI NARENDER <thodetinarender51@gmail.com>
2025-02-22 21:59:57 +0530

    S3Client updated

--- Expense Tracker/controllers/userController.js
+++ Expense Tracker/controllers/userController.js
@@ -214,15 +214,14 @@ const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
 const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
 //const { expenseReportt } = require('./models');  // Adjust the path to your model
 
-
-// AWS S3 client setup
 const s3 = new S3Client({
-    region: process.env.AWS_REGION,
+    region: process.env.AWS_REGION || "ap-south-1", // Set default if undefined
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID || "AKIAZI2LDRTUCAV6NYPQ", 
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "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",
     }
 });
+
 
 // Download expense report and upload to S3
 const downloadExpense = async (req, res) => {
